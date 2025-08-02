26de8277b5427bdd260db7a9d2a62f2e93801e4e
SriVarshini72 <attepuvarshini@gmail.com>
2025-08-02 09:48:55 +0530

    Add files via upload

---
+++ Length.java
@@ -0,0 +1,10 @@
+package example;
+
+public class Length {
+	public static void main(String[] args) {
+        // Creating StringBuffer
+        StringBuffer sb = new StringBuffer("Hello");
+        int len = sb.length();
+        System.out.println("Length: " + len);
+	}
+}
